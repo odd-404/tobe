586ceafe6816d83ef2d6b3b2a5d60ed0da886b19
odd-404 <odd8@qq.com>
2024-12-23 23:32:45 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase
 import { getDatabase } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-database.js";
 
 const firebaseConfig = {
-  ...
+  apiKey: "AIzaSyCeuW4TN7TpDpSNrno9FMfxh3RdrYLjm6o",
+  authDomain: "tobe-odd.firebaseapp.com",
+  projectId: "tobe-odd",
+  storageBucket: "tobe-odd.firebasestorage.app",
+  messagingSenderId: "264520250425",
+  appId: "1:264520250425:web:888f22708fd987135989e8",
+  measurementId: "G-XJKZLKD1KJ"
 };
 
 export const app = initializeApp(firebaseConfig);
